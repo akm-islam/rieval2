057120bc2f9879cb44870c3b4e5896de697e9f1c
akm-islam <mamunjahir.com@gmail.com>
2021-07-06 08:34:03 -0400

    ra

--- src/components/Slopecharts/Model/slopechart1.js
+++ src/components/Slopecharts/Model/slopechart1.js
@@ -95,19 +95,20 @@ export function CreatexpChart(selected_instances, sorted_features, lime_data, se
   var height = parent_height - margin.item_top_margin - margin.bottom;
   
   var parent_svg=d3.select("#exp_container")
-  parent_svg.selectAll(".ylabels").data([[.2, "++"], [.4, "+"],[.5, "0"], [.6, "-"], [.8, "--"]]).join('text').attr('class', 'ylabels')
-  .attr("y", d => d[0] * height).attr("x", margin.item_left_margin-5)// change x and text-anchor together
+  parent_svg.selectAll(".ylabels").data([[.125, "++"], [.375, "+"],[.5, "0"], [.625, "-"], [.875, "--"]]).join('text').attr('class', 'ylabels')
+  .attr("y", d => d[0] * item_height).attr("x", margin.item_left_margin-5)// change x and text-anchor together
   .attr('text-anchor', 'end').attr('dominant-baseline', 'hanging')
   .text(d => d[1]).attr("font-size", 14).attr('fill', "#636363")
 
   parent_svg.selectAll('.title_rect').data(sorted_features).join('rect').attr('class','title_rect').attr('width',item_width+2).attr('height',margin.item_top_margin).attr('x',(d,i)=>margin.item_left_margin+item_width*i+(margin.item_right_margin*i-1)).attr('fill','#d1d1d1')
   parent_svg.selectAll(".title_text").data(sorted_features).join('text').attr('class', 'title_text').text(d=>d[0]).attr('y', 0).attr('dominant-baseline', 'hanging').attr("font-size", 12).attr('x',(d,i)=>(margin.item_left_margin+margin.item_right_margin*i)+item_width*i+item_width/2).attr('text-anchor','middle')
-  parent_svg.selectAll('.item_rect').data(sorted_features).join('rect').attr('class','item_rect').attr('width',item_width).attr('height',item_height).attr('x',(d,i)=>margin.item_left_margin+item_width*i+(margin.item_right_margin*i)).attr('y',margin.item_top_margin).attr('fill','#d1d1d1').attr('fill-opacity', 0).attr('stroke','#b2b0b0')
+  parent_svg.selectAll('.item_rect').data(sorted_features).join('rect').attr('class','item_rect').attr('width',item_width).attr('height',item_height).attr('x',(d,i)=>margin.item_left_margin+item_width*i+(margin.item_right_margin*i))
+  .attr('y',margin.item_top_margin).attr('fill','#d1d1d1').attr('fill-opacity', 0).attr('stroke','#b2b0b0')
   
-  .attr('add_circle', function (d,svg_index) {
-    parent_svg.selectAll(".mylines"+svg_index).data([[.3], [.5],[.7]]).join('line').attr('class', 'mylines'+svg_index)
-    .attr('x1',(item_width*svg_index)+margin.item_left_margin).attr('x2',item_width)
-    .attr('y1',d=>d*item_height).attr('y2',d=>d[0]*item_height).attr('stroke-width',1).attr("stroke", "#707070")
+  .attr('add_circle_rect_line', function (d,svg_index) {
+    parent_svg.selectAll(".mylines"+svg_index).data([[.25], [.5],[.75]]).join('line').attr('class', 'mylines'+svg_index)
+    .attr('x1',margin.item_left_margin+item_width*svg_index+(margin.item_right_margin*svg_index)).attr('x2',margin.item_left_margin+item_width*svg_index+(margin.item_right_margin*svg_index+item_width))
+    .attr('y1',d=>d*(item_height+margin.item_top_margin)).attr('y2',d=>d*(item_height+margin.item_top_margin)).attr('stroke-width',1).attr("stroke", "#707070")
 
     var feature_name=d[0]
     var feature_contrib_name = d[0] + "_contribution"
