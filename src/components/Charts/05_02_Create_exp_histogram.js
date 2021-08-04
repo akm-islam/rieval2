d3220fc97e6a748d086574ce42898b56ab3725e5
akm-islam <mamunjahir.com@gmail.com>
2021-08-04 09:55:19 -0400

    ra

--- src/components/Charts/05_02_Create_exp_histogram.js
+++ src/components/Charts/05_02_Create_exp_histogram.js
@@ -4,73 +4,28 @@ export default function CreatexpHistogram(d, selection, selected_instances, sort
     var item_width = d3.select("#svg2").attr("width")
     var item_height = d3.select("#svg2").attr("height")
     var feature_name = d[0]
-    var feature_contrib_name = d[0] + "_contribution"
-
     defualt_models.map(model => {
-        var circ_data = []
+        var hist_data = []
         lime_data[model].map(item => {
             if (item['1-qid'] == selected_year && selected_instances.includes(parseInt(item['two_realRank']))) {
-                item['id'] = item['State'].replace(/ /g, '').replace(/[^a-zA-Z ]/g, "") + model.replace(/ /g, '').replace(/[^a-zA-Z ]/g, "")
-                circ_data.push(item)
+                hist_data.push(parseFloat(item[feature_name]))
+                hist_data.push(item)
             }
         })
         // Draw circle starts here
-        /*
-        var yScale = d3.scaleLinear().domain([d3.min(circ_data.map(item => parseFloat(item[d[0]]))), d3.max(circ_data.map(item => parseFloat(item[d[0]])))])
+        var y = d3.scaleLinear().domain([d3.min(hist_data.map(item => parseFloat(item[d[0]]))), d3.max(hist_data.map(item => parseFloat(item[d[0]])))])
             .range([margin.item_top_margin, item_height - margin.item_bottom_margin])
-        var xScale = d3.scaleLinear().domain([d3.min(circ_data.map(item => parseFloat(item[feature_contrib_name]))), d3.max(circ_data.map(item => parseFloat(item[feature_contrib_name])))])
+        var x = d3.scaleLinear().domain([d3.min(hist_data.map(item => parseFloat(item[feature_name]))), d3.max(hist_data.map(item => parseFloat(item[feature_name])))])
             .range([margin.item_left_margin, item_width - margin.item_right_margin])
-*/
-        var margin = { top: 20, right: 30, bottom: 40, left: 90 },
-            width = item_width - margin.left - margin.right,
-            height = item_height - margin.top - margin.bottom;
-
-        // append the svg object to the body of the page
-        var svg=selection.append("g")
-            .attr("transform",
-                "translate(" + margin.left + "," + margin.top + ")");
-
-        // Parse the Data
-        d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/7_OneCatOneNum_header.csv", function (data) {
         
-            // Add X axis
-            var x = d3.scaleLinear()
-                .domain([0, 13000])
-                .range([0, width]);
-            svg.append("g")
-                .attr("transform", "translate(0," + height + ")")
-                .call(d3.axisBottom(x))
-                .selectAll("text")
-                .attr("transform", "translate(-10,0)rotate(-45)")
-                .style("text-anchor", "end");
+        // set the parameters for the histogram
+        var histogram = d3.histogram()
+            .value(function (d) { return d })   // I need to give the vector of value
+            .thresholds(x.ticks(7)); // then the numbers of bins
 
-            // Y axis
-            var y = d3.scaleBand()
-                .range([0, height])
-                .domain(data.map(function (d) { return d.Country; }))
-                .padding(.1);
-          
-
-            //Bars
-            svg.selectAll("myRect")
-                .data(data)
-                .enter()
-                .append("rect")
-                .attr("x", x(0))
-                .attr("y", function (d) { return y(d.Country); })
-                .attr("width", function (d) { return x(d.Value); })
-                .attr("height", y.bandwidth())
-                .attr("fill", "#69b3a2")
-
-
-            // .attr("x", function(d) { return x(d.Country); })
-            // .attr("y", function(d) { return y(d.Value); })
-            // .attr("width", x.bandwidth())
-            // .attr("height", function(d) { return height - y(d.Value); })
-            // .attr("fill", "#69b3a2")
-
-        })
-
+        // And apply this function to data to get the bins
+        var bins = histogram(hist_data);
+        console.log(bins)
 
 
     })
