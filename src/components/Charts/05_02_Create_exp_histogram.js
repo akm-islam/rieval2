b03c2a8f1ce28589896736b8ec140bf9cc136812
AKM Zahirul Islam <mamunjahir.com@gmail.com>
2021-08-04 21:42:00 +0600

    ra

--- src/components/Charts/05_02_Create_exp_histogram.js
+++ src/components/Charts/05_02_Create_exp_histogram.js
@@ -8,7 +8,7 @@ export default function CreatexpHistogram(d, selection, selected_instances, sort
         var hist_data = []
         lime_data[model].map(item => {
             if (item['1-qid'] == selected_year && selected_instances.includes(parseInt(item['two_realRank']))) {
-                hist_data.push(parseFloat(item[feature_name]))
+                item[feature_name] = parseFloat(item[feature_name])
                 hist_data.push(item)
             }
         })
@@ -17,16 +17,26 @@ export default function CreatexpHistogram(d, selection, selected_instances, sort
             .range([margin.item_top_margin, item_height - margin.item_bottom_margin])
         var x = d3.scaleLinear().domain([d3.min(hist_data.map(item => parseFloat(item[feature_name]))), d3.max(hist_data.map(item => parseFloat(item[feature_name])))])
             .range([margin.item_left_margin, item_width - margin.item_right_margin])
-        
+
         // set the parameters for the histogram
         var histogram = d3.histogram()
-            .value(function (d) { return d })   // I need to give the vector of value
-            .thresholds(x.ticks(7)); // then the numbers of bins
-
+            .value(d => d[feature_name])   // I need to give the vector of value
+            .domain(x.domain())  // then the domain of the graphic
+            .thresholds(x.ticks(3)); // then the numbers of bins
         // And apply this function to data to get the bins
         var bins = histogram(hist_data);
-        console.log(bins)
-
+        console.log(d[0],bins, hist_data)
+        // append the bar rectangles to the svg element
+        selection.selectAll("rect")
+            .data(bins)
+            .enter()
+            .append("rect")
+            .attr("x", 1)
+            .attr("transform", function (d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
+            .attr("width", function (d) { return x(d.x1) - x(d.x0) - 1; })
+            .attr("height", function (d) { return item_height - y(d.length); })
+            .style("fill", "#69b3a2")
+            .attr("length",d=>d.length)
 
     })
 
