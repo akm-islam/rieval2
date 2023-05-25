d433f9edb0117582972d43b2890f7dd346e088f6
Kaustav Bhattacharjee <kaustavbhattacharjee@users.noreply.github.com>
2023-05-25 14:28:16 -0400

    fix: updated fiscal_ice_may25.csv

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import rur from "./Data/RUR/RUR.csv";
 
 import fiscal_rank from "./Data/data/fiscal/lime/fiscal_rank.csv";
 import fiscal_lime from "./Data/data/fiscal/lime/fiscal_lime_may25.csv";
-import fiscal_ice from "./Data/data/fiscal/ice/fiscal_ice.csv";
+import fiscal_ice from "./Data/data/fiscal/ice/fiscal_ice_may25.csv";
 
 import school_rank from "./Data/data/school/lime/school_rank.csv";
 import school_lime from "./Data/data/school/lime/school_lime.csv";
